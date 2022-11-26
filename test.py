629f504d2fa3b23143751de8383f85f5fd993075
avivzusim <84519703+avivzusim@users.noreply.github.com>
2022-11-26 20:14:56 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ import pyweb as web
 
 main = "web.html"
 
+#set log True (Print everything that happens for people who wanna look like hackers)
+web.logs = True
+
 # print the pages dict (all program pages)
 print(web.pages)
 
@@ -9,7 +12,14 @@ print(web.pages)
 web.page.new(main,"something.css","title") #log: pages in the whole program
 
 # name, page to edit: create new div.
-web.div.new("a", main)
+web.div.open("a", main)
+# close div on file
+web.div.close(main)
+
+
+# loop for objects!   
+for i in range(0,2):
+    web.text.new(main,str(i),"h2")
 
 # create new paragraph:
 web.page.p(main,"text")
@@ -17,7 +27,23 @@ web.page.p(main,"text")
 # create image (page, source, xy size)
 web.page.img(main,"img.png","555x800")
 
+# create text with sizes {h1","h2","h3","h4","h5","h6}
+web.text.new(main, "Hello World", "h1")
 
+            #create table later -- stupid lol - for aviv () --
+
+
+# create style tag in *html*
+web.css.style_tag(main)
+
+#write stuff urself lol
+web.page.write(main,"""body{
+            text-align: center;
+            background-color: red;
+        }""")
+
+# finish style tag in *html*
+web.css.finish_tag(main)
 
 
 # web finish (</html></body>)
